ff5f5cbeae9177fb6e7449f96115de2997da9446
高原 <gaoyuan@yiliangche.net>
2016-06-23 15:37:27 +0800

    1: add web mode;2 add rust mode

---
+++ modules/goblin-rust.el
@@ -0,0 +1,95 @@
+;;; goblin-php.el
+
+;;; Copyright © 2014 jqlblue <gaoyuan.blue@gmail.com>
+
+;;; URL: https://github.com/jqlblue/goblin-emacs
+
+;;; Commentary:
+
+;; The package is fork from http://batsov.com/prelude/, enhance in web development.
+
+;;; License:
+
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 2.1 of the License, or (at your option) any later version.
+;; 
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;; 
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301  USA
+
+;;; Code:
+
+;(global-company-mode)
+
+(setenv "PATH" (concat "/usr/local/bin" ":"
+                       "~/.cargo/bin" ":" 
+                       (getenv "PATH")))
+(setq exec-path (append exec-path '("/usr/local/bin" "~/.cargo/bin")))
+
+;; Reduce the time after which the company auto completion popup opens
+(setq company-idle-delay 0.2)
+
+;; Reduce the number of characters before company kicks in
+(setq company-minimum-prefix-length 1)
+
+;; Set path to racer binary
+(setq racer-cmd "~/.cargo/bin/racer")
+(setq rustfmt-bin "~/.cargo/bin/rustfmt")
+
+;; Set path to rust src directory
+(setq racer-rust-src-path "~/.rust/src")
+
+;; Load rust-mode when you open `.rs` files
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(require 'company-racer)
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'company-racer))
+
+(unless (getenv "RUST_SRC_PATH")
+  (setenv "RUST_SRC_PATH" (expand-file-name "~/.rust/src")))
+
+(add-hook 'rust-mode-hook  #'racer-mode)
+(add-hook 'rust-mode-hook  #'flycheck-mode)
+(add-hook 'rust-mode-hook  #'cargo-minor-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(add-hook 'rust-mode-hook  #'rustfmt-enable-on-save)
+
+	;(add-hook 'after-init-hook #'global-flycheck-mode)
+;; Setting up configurations when you load rust-mode
+
+(defun indent-buffer ()
+  "Indent current buffer according to major mode."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(add-hook 'rust-mode-hook
+          '(lambda ()
+             
+             (define-key rust-mode-map (kbd "C-c C-f") #'rustfmt-format-buffer)
+            
+             ;; Use flycheck-rust in rust-mode
+             (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+             
+             ;; Use company-racer in rust mode
+             (set (make-local-variable 'company-backends) '(company-racer))
+             
+             ;; Key binding to jump to method definition
+             (local-set-key (kbd "M-.") #'racer-find-definition)
+             
+             ;; Key binding to auto complete and indent
+             (local-set-key (kbd "TAB") #'company-indent-or-complete-common)
+             ))
+
+(provide 'goblin-rust)
+
+;;; goblin-rust.el ends here
---
+++ modules/goblin-web.el
@@ -0,0 +1,59 @@
+;;; goblin-web.el
+
+;;; Copyright © 2016 jqlblue <gaoyuan.blue@gmail.com>
+
+;;; URL: https://github.com/jqlblue/goblin-emacs
+
+;;; Commentary:
+
+;; The package is fork from http://batsov.com/prelude/, enhance in web development.
+
+;;; License:
+
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 2.1 of the License, or (at your option) any later version.
+;; 
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;; 
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301  USA
+
+;;; Code:
+
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 4)
+  )
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+
+(setq web-mode-markup-indent-offset 2)
+
+(setq web-mode-css-indent-offset 2)
+
+
+(setq web-mode-style-padding 1)
+
+
+
+(provide 'goblin-web)
+
+;;; goblin-web.el ends here
